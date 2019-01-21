176c27750e71bab625ace41842b3949d58798b81
王艳芬 <wangyanfen@didichuxing.com>
2019-01-21 17:40:22 +0800

    初始化

---
+++ src/ui.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+
+import {
+  /* eslint-disable no-unused-vars */
+  Style,
+  // basic
+  Button,
+  Loading,
+  Tip,
+  Toast,
+  Dialog,
+  Scroll
+} from 'cube-ui'
+
+Vue.use(Button)
+Vue.use(Loading)
+Vue.use(Tip)
+Vue.use(Toast)
+Vue.use(Scroll)
+Vue.use(Dialog)
